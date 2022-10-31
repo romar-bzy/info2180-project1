beee42092fc7235567c013941e503bf2aa24f231
Romar Simms <romarsimms.rs@gmail.com>
2022-10-30 19:27:45 -0500

    Edited

--- scripts.js
+++ scripts.js
@@ -1,1 +1,26 @@
-/* Add your JavaScript to this file */
+function sprinkle(){
+    var emadd = document.getElementsByName("email");
+    document.getElementsByClassName("btn")[1].onclick=function(e){
+
+        var input = emadd.value;
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if (input == "") {
+            document.getElementsByClassName("message").innerHTML = "Please provide a valid email address";
+        }
+
+        else if (!input.match(mailformat)) {
+            document.getElementsByClassName("message").innerHTML = "Please provide a valid email address";
+        }
+
+        else if (input.includes("@") && input.includes(".")) {
+            document.getElementsByClassName("message").innerHTML = "Thank You! Your email address" + " " + eaddress + " " + "has been added to our mailing list";    
+        }
+
+        
+    } 
+    e.preventDefault();
+    
+}
+
+window.onload = sprinkle;
